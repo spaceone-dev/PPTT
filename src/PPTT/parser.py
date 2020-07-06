ef4df45f8d37fa220a652baeb3bd56af270bd97f
sinsky <sinsky@magazone.com>
2020-07-06 12:14:05 +0900

    fix flake8 issue

--- src/PPTT/parser.py
+++ src/PPTT/parser.py
@@ -1,10 +1,9 @@
 from pptx import Presentation
 
-from .stub import PPTTemplateStub
 from .schema import PPTTemplate
 
 
-def parser(file: str)->PPTTemplate:
+def parser(file: str) -> PPTTemplate:
     master_ppt = Presentation(file)
     template = PPTTemplate.load_by_pptx(master_ppt)
     return template
